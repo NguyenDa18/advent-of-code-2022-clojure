ee4f7b71d60c3a764b67fa178637b9d425493708
Danh Nguyen <danhnguyen267@gmail.com>
2022-12-05 22:26:42 -0800

    Starting day5 puzzle, demo finished with unit test

--- src/advent_of_code/day5.clj
+++ src/advent_of_code/day5.clj
@@ -1,23 +1,41 @@
 (ns advent-of-code.day5
     (:require [clojure.string :as s]))
 
+; Sources
+; https://clojuredocs.org/clojure.core/drop
+; https://clojuredocs.org/clojure.core/reverse
+
+"""
+Advent of Code Day 5: https://adventofcode.com/2022/day/5
+"""
 (def crate-stack [["Z","N"], ["M","C","D"],["P"]])
 
 (defn rearrange-crates [crates-stack, move-amt, move-start, move-end]
-    (let [start-idx (- move-start 1)
+    (let [
+        start-idx (- move-start 1)
          end-idx (- move-end 1)
          start-stack (get crates-stack start-idx)
          end-stack (get crates-stack end-idx)
          start-removed-amt (- (count start-stack) move-amt)
-         start-removed-crates (vec (drop start-removed-amt start-stack))
+         start-removed-crates (reverse (drop start-removed-amt start-stack))
          start-stack-post (drop-last move-amt start-stack)
          end-stack-post (concat end-stack start-removed-crates)
          updated-start (assoc crates-stack start-idx start-stack-post)
          res (assoc updated-start end-idx end-stack-post)]
     res))
 
-(defn solve
+(defn solve-part1-demo
     [crates-stack]
-    (let [crates (atom crates-stack)
-        crates (reset! crates (rearrange-crates @crates 1 2 1))]
-    crates))
+    (let [crates (atom crates-stack)]
+        (reset! crates (rearrange-crates @crates 1 2 1))
+        (reset! crates (rearrange-crates @crates 3 1 3))
+        (reset! crates (rearrange-crates @crates 2 2 1))
+        (reset! crates (rearrange-crates @crates 1 1 2))))
+
+(defn solve-part1-puzzle
+    [crates-stack]
+    (let [crates (atom crates-stack)]
+        (reset! crates (rearrange-crates @crates 3 8 9))
+        (reset! crates (rearrange-crates @crates 2 2 8))
+        (reset! crates (rearrange-crates @crates 5 4 2))
+    ))
--- test/advent_of_code/day5_test.clj
+++ test/advent_of_code/day5_test.clj
@@ -3,8 +3,25 @@
             [clojure.string :as s]
             [advent-of-code.day5 :as d5]))
 
-(def crate-stack [["Z","N"], ["M","C","D"],["P"]])
+(def crate-stack-part1-demo [["Z","N"], ["M","C","D"],["P"]])
+
+(def crate-stack-part1 [
+    ["P", "F", "M", "Q", "W", "G", "R", "T"],
+    ["H", "F", "R"],
+    ["P", "Z", "R", "V", "G", "H", "S", "D"],
+    ["Q", "H", "P", "B", "F", "W", "G"],
+    ["P", "S", "M", "J" "H"]
+    ["M", "Z" "T" "H" "S" "R" "P" "L"],
+    ["P" "T" "H" "N" "M" "L"],
+    ["F" "D" "Q" "R"]
+    ["D" "S" "C" "N" "L" "P" "H"]
+])
+
+(def day5-part-1-input (s/split-lines (slurp "src/advent_of_code/data/day5-moves.txt")))
+
+(def day5-part-1-moves
+    (map #(s/split % #"") day5-part-1-input))
 
 (deftest day5
-    (testing "Test for day5 part 1"
-        (is (= [["Z", "N", "D"], ["M", "C"], ["P"]] (d5/solve crate-stack)))))
+    (testing "Test for day5 part 1 demo"
+        (is (= [["C"], ["M"], ["P" "D" "N" "Z"]] (d5/solve-part1-demo crate-stack-part1-demo)))))
